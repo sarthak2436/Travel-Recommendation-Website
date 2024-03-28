33cb7a5d19d847dbf4dc55534333f1db120d2053
sarthak2436 <154762468+sarthak2436@users.noreply.github.com>
2024-03-29 01:16:43 +0530

    Add files via upload

---
+++ api-key.js
@@ -0,0 +1,3 @@
+
+
+let apiKey = "1cfec89c4caa16ff837587be";
---
+++ video.js
@@ -0,0 +1,23 @@
+function toggleVideo() {
+    const trailer = document.querySelector('.trailer');
+    const video = document.querySelector('video');
+    video.pause();
+    trailer.classList.toggle('active');
+}
+function changeBg(bg, title) {
+    const banner = document.querySelector('.banner');
+    const contents = document.querySelector('.contents');
+    banner.style.backround=`url("${bg}")`;
+    banner.style.backgroundSize = 'cover'
+    banner.style.backgroundPosition = 'center';
+
+    contents.forEach(content => {
+        content.classList.remove('active');
+        if (content.classList.contains(title)) {
+            content.classList.add('active')
+        }
+
+
+    });
+
+}
